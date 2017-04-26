ca20c8aa01c2725e3e9e7785e6478e2c7759e763
Ivan Ermolaev <ermolaevym@gmail.com>
2017-04-26 22:44:11 +0300

    added js features

--- recruiter-view/src/app/app.module.ts
+++ recruiter-view/src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { AuthorizationComponent } from './components/authorization.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AuthorizationComponent
   ],
   imports: [
     BrowserModule,
